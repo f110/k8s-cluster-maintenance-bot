570b8f123e47e44e360535ec7f0c29e92132ad56
Fumihiro Ito <fmhrit@gmail.com>
2020-02-08 22:14:38 +0900

    Remove unnecessary functions

--- pkg/consumer/build.go
+++ pkg/consumer/build.go
@@ -1,14 +1,11 @@
 package consumer
 
 import (
-	"bytes"
 	"fmt"
-	"html/template"
 	"math/rand"
 	"os"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"golang.org/x/xerrors"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -29,14 +26,6 @@ const (
 
 	labelKeyJobId = "k8s-cluster-maintenance-bot.f110.dev/job-id"
 )
-
-const dockerPushScript = `until docker ps
-do
-	sleep 3
-done
-docker load -i {{ .ArtifactPath }}
-docker images
-`
 
 var letters = "abcdefghijklmnopqrstuvwxyz1234567890"
 
@@ -104,31 +93,6 @@ func (b *BazelBuild) cleanup(client *kubernetes.Clientset, buildId string) error
 	return nil
 }
 
-func (b *BazelBuild) shipArtifact(client *kubernetes.Clientset, buildId string) error {
-	shipPod := b.buildShipPod(buildId)
-	_, err := client.CoreV1().Pods(os.Getenv("POD_NAMESPACE")).Create(shipPod)
-	if err != nil {
-		return xerrors.Errorf(": %v", err)
-	}
-	watchCh, err := client.CoreV1().Pods(os.Getenv("POD_NAMESPACE")).Watch(metav1.ListOptions{
-		FieldSelector: fmt.Sprintf("metadata.name=%s", shipPod.ObjectMeta.Name),
-	})
-	if err != nil {
-		return xerrors.Errorf(": %v", err)
-	}
-	defer watchCh.Stop()
-
-Watch:
-	for e := range watchCh.ResultChan() {
-		switch e.Type {
-		case watch.Deleted:
-			break Watch
-		}
-	}
-
-	return nil
-}
-
 func (b *BazelBuild) buildRepository(client *kubernetes.Clientset, buildId string) error {
 	buildPod := b.buildPod(buildId)
 	_, err := client.CoreV1().Pods(os.Getenv("POD_NAMESPACE")).Create(buildPod)
@@ -160,132 +124,6 @@ Watch:
 	}
 
 	return nil
-}
-
-func (b *BazelBuild) buildShipPod(buildId string) *corev1.Pod {
-	buf := new(bytes.Buffer)
-	t := template.Must(template.New("").Parse(dockerPushScript))
-	err := t.Execute(buf, struct {
-		ArtifactPath string
-	}{
-		ArtifactPath: "/work/image.tar",
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	return &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      fmt.Sprintf("%s-%s-ship", b.Rule.Name, buildId),
-			Namespace: os.Getenv("POD_NAMESPACE"),
-			Annotations: map[string]string{
-				"script": buf.String(),
-			},
-		},
-		Spec: corev1.PodSpec{
-			ServiceAccountName: builderServiceAccount,
-			RestartPolicy:      corev1.RestartPolicyNever,
-			InitContainers: []corev1.Container{
-				{
-					Name:  "pre-process",
-					Image: buildSidecarImage,
-					Args: []string{
-						"--action=download-artifacts",
-						fmt.Sprintf("--artifact-host=%s", artifactHost),
-						fmt.Sprintf("--artifact-bucket=%s", artifactBucket),
-						"--artifact-path=/work",
-					},
-					Env: []corev1.EnvVar{
-						{Name: "POD_NAME", ValueFrom: &corev1.EnvVarSource{
-							FieldRef: &corev1.ObjectFieldSelector{
-								FieldPath: "metadata.name",
-							},
-						}},
-						{Name: "POD_NAMESPACE", ValueFrom: &corev1.EnvVarSource{
-							FieldRef: &corev1.ObjectFieldSelector{
-								FieldPath: "metadata.namespace",
-							},
-						}},
-						{Name: "AWS_ACCESS_KEY_ID", ValueFrom: &corev1.EnvVarSource{
-							SecretKeyRef: &corev1.SecretKeySelector{
-								LocalObjectReference: corev1.LocalObjectReference{
-									Name: storageTokenSecretName,
-								},
-								Key: "accesskey",
-							},
-						}},
-						{Name: "AWS_SECRET_ACCESS_KEY", ValueFrom: &corev1.EnvVarSource{
-							SecretKeyRef: &corev1.SecretKeySelector{
-								LocalObjectReference: corev1.LocalObjectReference{
-									Name: storageTokenSecretName,
-								},
-								Key: "secretkey",
-							},
-						}},
-						{Name: "JOB_NAME", Value: b.Rule.Name},
-						{Name: "JOB_ID", Value: buildId},
-					},
-					VolumeMounts: []corev1.VolumeMount{
-						{Name: "workdir", MountPath: "/work"},
-					},
-				},
-			},
-			Containers: []corev1.Container{
-				{
-					Name:    "main",
-					Image:   "docker:17.10",
-					Command: []string{"sh"},
-					Args:    []string{"/script/script"},
-					Env: []corev1.EnvVar{
-						{Name: "DOCKER_HOST", Value: "127.0.0.1"},
-					},
-					WorkingDir: "/work",
-					VolumeMounts: []corev1.VolumeMount{
-						{Name: "workdir", MountPath: "/work"},
-						{Name: "script", MountPath: "/script"},
-					},
-				},
-				{
-					Name:  "dind",
-					Image: "docker:17.10-dind",
-					SecurityContext: &corev1.SecurityContext{
-						Privileged: aws.Bool(true),
-					},
-				},
-				{
-					Name:  "wait",
-					Image: buildSidecarImage,
-					Args:  []string{"--action=wait"},
-					Env: []corev1.EnvVar{
-						{Name: "POD_NAME", ValueFrom: &corev1.EnvVarSource{
-							FieldRef: &corev1.ObjectFieldSelector{
-								FieldPath: "metadata.name",
-							},
-						}},
-						{Name: "POD_NAMESPACE", ValueFrom: &corev1.EnvVarSource{
-							FieldRef: &corev1.ObjectFieldSelector{
-								FieldPath: "metadata.namespace",
-							},
-						}},
-					},
-				},
-			},
-			Volumes: []corev1.Volume{
-				{Name: "workdir", VolumeSource: corev1.VolumeSource{
-					EmptyDir: &corev1.EmptyDirVolumeSource{},
-				}},
-				{Name: "script", VolumeSource: corev1.VolumeSource{
-					DownwardAPI: &corev1.DownwardAPIVolumeSource{
-						Items: []corev1.DownwardAPIVolumeFile{
-							{Path: "script", FieldRef: &corev1.ObjectFieldSelector{
-								FieldPath: "metadata.annotations['script']",
-							}},
-						},
-					},
-				}},
-			},
-		},
-	}
 }
 
 func (b *BazelBuild) buildPod(buildId string) *corev1.Pod {
