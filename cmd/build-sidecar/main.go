39484a9275b665f275724635e0a6600caa762174
Fumihiro Ito <fmhrit@gmail.com>
2020-03-01 22:22:12 +0900

    sidecar: Checkout specific revision by an archive file when repository hosted by github

--- cmd/build-sidecar/main.go
+++ cmd/build-sidecar/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"archive/tar"
 	"bytes"
+	"compress/gzip"
 	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
@@ -19,10 +21,12 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/bradleyfalzon/ghinstallation"
+	"github.com/google/go-github/v29/github"
 	"github.com/spf13/pflag"
 	"golang.org/x/xerrors"
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport"
 	gogitHttp "gopkg.in/src-d/go-git.v4/plumbing/transport/http"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -42,16 +46,37 @@ const (
 )
 
 func actionClone(appId, installationId int64, privateKeyFile, dir, repo, commit string) error {
-	t, err := ghinstallation.NewKeyFromFile(http.DefaultTransport, appId, installationId, privateKeyFile)
-	if err != nil {
-		return xerrors.Errorf(": %v", err)
-	}
-	token, err := t.Token(context.Background())
-	if err != nil {
-		return xerrors.Errorf(": %v", err)
+	var auth *gogitHttp.BasicAuth
+	rt := http.DefaultTransport
+	if _, err := os.Stat(privateKeyFile); !os.IsNotExist(err) {
+		t, err := ghinstallation.NewKeyFromFile(http.DefaultTransport, appId, installationId, privateKeyFile)
+		if err != nil {
+			return xerrors.Errorf(": %v", err)
+		}
+		token, err := t.Token(context.Background())
+		if err != nil {
+			return xerrors.Errorf(": %v", err)
+		}
+		auth = &gogitHttp.BasicAuth{Username: "octocat", Password: token}
+		rt = t
 	}
 
-	depth := 1
+	archiveDownload := false
+	u, err := url.Parse(repo)
+	if err == nil {
+		if u.Scheme == "https" && u.Hostname() == "github.com" {
+			archiveDownload = true
+		}
+	}
+
+	if commit != "" && archiveDownload {
+		return checkoutCommit(dir, repo, commit, rt)
+	} else {
+		return cloneByGit(dir, repo, commit, 1, auth)
+	}
+}
+
+func cloneByGit(dir, repo, commit string, depth int, auth transport.AuthMethod) error {
 	if commit != "" {
 		depth = 0
 	}
@@ -59,7 +84,7 @@ func actionClone(appId, installationId int64, privateKeyFile, dir, repo, commit 
 	r, err := git.PlainClone(dir, false, &git.CloneOptions{
 		URL:   repo,
 		Depth: depth,
-		Auth:  &gogitHttp.BasicAuth{Username: "octocast", Password: token},
+		Auth:  auth,
 	})
 	if err != nil {
 		return xerrors.Errorf(": %v", err)
@@ -71,6 +96,83 @@ func actionClone(appId, installationId int64, privateKeyFile, dir, repo, commit 
 			return xerrors.Errorf(": %v", err)
 		}
 		if err := tree.Checkout(&git.CheckoutOptions{Hash: plumbing.NewHash(commit)}); err != nil {
+			return xerrors.Errorf(": %v", err)
+		}
+	}
+
+	return nil
+}
+
+func checkoutCommit(dir, u, commit string, rt http.RoundTripper) error {
+	addr := u
+	if strings.HasSuffix(u, ".git") {
+		addr = strings.TrimSuffix(u, ".git")
+	}
+	parsed, err := url.Parse(addr)
+	if err != nil {
+		return xerrors.Errorf(": %v", err)
+	}
+	s := strings.SplitN(parsed.Path, "/", 3)
+
+	ghClient := github.NewClient(&http.Client{Transport: rt})
+	archiveLink, _, _ := ghClient.Repositories.GetArchiveLink(
+		context.Background(),
+		s[1], // owner
+		s[2], // repo
+		github.Tarball,
+		&github.RepositoryContentGetOptions{Ref: commit},
+		true,
+	)
+
+	log.Printf("Download archive from %s", archiveLink.String())
+	req, err := http.NewRequest(http.MethodGet, archiveLink.String(), nil)
+	if err != nil {
+		return xerrors.Errorf(": %v", err)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return xerrors.Errorf(": %v", err)
+	}
+	defer res.Body.Close()
+
+	gzReader, err := gzip.NewReader(res.Body)
+	if err != nil {
+		return xerrors.Errorf(": %v", err)
+	}
+	tarReader := tar.NewReader(gzReader)
+	for {
+		h, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return xerrors.Errorf(": %v", err)
+		}
+		d, f := filepath.Split(h.Name)
+		if d == "" {
+			continue
+		}
+		s := strings.Split(d, "/")
+		filename := filepath.Join(dir, strings.Join(s[1:], "/"), f)
+
+		if h.Typeflag == tar.TypeDir {
+			if err := os.MkdirAll(filename, os.FileMode(h.Mode)); err != nil {
+				return xerrors.Errorf(": %v", err)
+			}
+			continue
+		}
+
+		b, err := ioutil.ReadAll(tarReader)
+		if err != nil {
+			return xerrors.Errorf(": %v", err)
+		}
+		dirname := filepath.Dir(filename)
+		if _, err := os.Stat(dirname); os.IsNotExist(err) {
+			if err := os.MkdirAll(dirname, 755); err != nil {
+				return xerrors.Errorf(": %v", err)
+			}
+		}
+		if err := ioutil.WriteFile(filename, b, os.FileMode(h.Mode)); err != nil {
 			return xerrors.Errorf(": %v", err)
 		}
 	}
